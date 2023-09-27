ba0abdb04a95570d9d3da105283311d5595957fb
Movses Martirosyan <movsesmartirosyan@Movsess-MacBook-Pro.local>
2023-09-27 14:51:05 +0400

    added comment to http provider

--- providers.tf
+++ providers.tf
@@ -3,11 +3,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-terraform {
-  required_providers {
+# http provider
+equired_providers {
     http = {
       source  = "hashicorp/http"
       version = "3.4.0"
     }
   }
-}
